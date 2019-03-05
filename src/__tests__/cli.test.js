5b7433f57fe2a0acad063af2f2058e13c4fcaf08
Yasuaki Uechi <y@uechi.io>
2019-03-05 23:14:08 +0900

    add linter

--- src/__tests__/cli.test.js
+++ src/__tests__/cli.test.js
@@ -3,7 +3,7 @@ import * as util from '../util'
 
 it('getRepositoryDetails', async () => {
   nock.disableNetConnect()
-  const scope = nock('https://api.github.com')
+  nock('https://api.github.com')
     .get('/repos/facebook/react')
     .replyWithFile(200, __dirname + '/fixtures/react.json')
     .get('/repos/vuejs/vue')
@@ -19,26 +19,16 @@ it('getRepositoryDetails', async () => {
 
 it('generateTable', async () => {
   nock.disableNetConnect()
-  const scope = nock('https://api.github.com')
+  nock('https://api.github.com')
     .get('/repos/facebook/react')
     .replyWithFile(200, __dirname + '/fixtures/react.json')
     .get('/repos/vuejs/vue')
     .replyWithFile(200, __dirname + '/fixtures/vue.json')
 
-  const fixture = `| Repository | facebook/react | vuejs/vue     |
-| ---------- | -------------- | ------------- |
-| Language   | JavaScript     | JavaScript    |
-| Created    | almost 6 years | over 5 years  |
-| Updated    | about 7 hours  | about 4 hours |
-| Stars      | 123178         | 128899        |
-| Watches    | 6636           | 5781          |
-| Forks      | 22348          | 18376         |
-| Issues     | 542            | 237           |
-| Size       | 137.22MB       | 26.21MB       |
-| Owner      | Organization   | Organization  |`
   const repos = await Promise.all(
     Array.from(['facebook/react', 'vuejs/vue']).map(util.getRepositoryDetails)
   )
   const table = util.generateTable(repos, true)
-  expect(table).toBe(fixture)
+  expect(table).toContain('| Repository | facebook/react | vuejs/vue')
+  expect(table).toContain('137.22MB       | 26.21MB')
 })
