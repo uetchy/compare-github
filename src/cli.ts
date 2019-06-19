efefabcecfcec8fd3143888b783359be9ffdc78c
Yasuaki Uechi <y@uechi.io>
2019-06-19 17:17:11 +0900

    fix: typed interface

--- src/cli.ts
+++ src/cli.ts
@@ -1,11 +1,10 @@
 #!/usr/bin/env node
-// tslint:disable:no-console
 
 import minimist from 'minimist'
 import ora from 'ora'
 import { generateTable, getRepositoryDetails } from './util'
 
-async function init(names: string[], enableMarkdown: boolean) {
+async function init(names: string[], enableMarkdown: boolean): Promise<void> {
   if (names.length === 0) {
     console.log('Usage: $ gh-compare vuejs/vue facebook/react')
     throw new Error('no repos given')
@@ -15,11 +14,13 @@ async function init(names: string[], enableMarkdown: boolean) {
       Array.from(new Set(names)).map(getRepositoryDetails)
     )
     const result = generateTable(repos, enableMarkdown)
+    // eslint-disable-next-line @typescript-eslint/no-use-before-define
     spinner.info(
       `Compared to ${repos.length} repositor${repos.length > 1 ? 'ies' : 'y'}`
     )
     console.log(result)
   } catch (err) {
+    // eslint-disable-next-line @typescript-eslint/no-use-before-define
     spinner.fail(err)
     throw new Error(err)
   }
--- src/util.ts
+++ src/util.ts
@@ -1,90 +1,30 @@
 import bytes from 'bytes'
 import chalk from 'chalk'
 import Table from 'cli-table'
-import distanceInWordsToNow from 'date-fns/distance_in_words_to_now'
-import parse from 'date-fns/parse'
+import { formatDistanceToNow, parseISO } from 'date-fns'
 import MarkdownTable from 'markdown-table'
 import fetch from 'node-fetch'
 
-interface IRepo {
+interface Repository {
   repository: string
   language: string
   created: number
   updated: number
-  stars: string
-  watches: string
-  forks: string
-  issues: string
-  size: string
+  stars: number
+  watches: number
+  forks: number
+  issues: number
+  size: number
   owner: string
-  [index: string]: any
 }
 
-export async function getRepositoryDetails(fullname: string): Promise<IRepo> {
-  const res = await fetch(`https://api.github.com/repos/${fullname}`)
-  if (parseInt(res.headers.get('x-ratelimit-remaining') as string, 10) === 0) {
-    const resetFrom = new Date(
-      parseInt(res.headers.get('x-ratelimit-reset') as string, 10) * 1000
-    )
-    throw new Error(
-      `Rate limit exceeded. It will be reset in ${distanceInWordsToNow(
-        resetFrom
-      )}`
-    )
-  }
-  if (res.status === 404) {
-    throw new Error(`Repository not found for ${fullname}`)
-  }
-
-  const json = await res.json()
-  return {
-    created: parse(json.created_at).getTime(),
-    forks: json.forks_count,
-    issues: json.open_issues_count, // issues + PRs
-    language: json.language,
-    owner: json.owner.type,
-    repository: json.full_name,
-    size: json.size,
-    stars: json.stargazers_count,
-    updated: parse(json.pushed_at).getTime(),
-    watches: json.subscribers_count,
-  }
-}
-
-export function generateTable(repos: IRepo[], markdownTable: boolean) {
-  const contents = Object.keys(repos[0]).map((key) => {
-    let values = repos.map((repo) => repo[key])
-    if (['stars', 'watches', 'forks', 'issues'].includes(key)) {
-      values = injectValues(values, false, !markdownTable)
-    } else if (key === 'size') {
-      values = injectValues(values, true, !markdownTable, (kb) =>
-        bytes(kb * 1024)
-      )
-    } else if (key === 'updated') {
-      values = injectValues(values, false, !markdownTable, distanceInWordsToNow)
-    } else if (key === 'created') {
-      values = injectValues(values, false, false, distanceInWordsToNow)
-    }
-    return [capitalize(key), ...values]
-  })
-
-  const table = markdownTable ? MarkdownTable(contents) : CLITable(contents)
-  return table
-}
-
-function CLITable(arr: any[]) {
-  const table = new Table()
-  table.push(...arr)
-  return table.toString()
-}
-
-function argminmax(arr: number[]) {
+function argminmax<T>(arr: T[]): { minIdx: number; maxIdx: number } {
   let deltaMin = arr[0]
   let deltaMax = arr[0]
   let deltaMinIdx = 0
   let deltaMaxIdx = 0
 
-  arr.forEach((el, idx) => {
+  arr.forEach((el, idx): void => {
     if (el < deltaMin) {
       deltaMin = el
       deltaMinIdx = idx
@@ -98,25 +38,109 @@ function argminmax(arr: number[]) {
   return { minIdx: deltaMinIdx, maxIdx: deltaMaxIdx }
 }
 
-function injectValues(
-  values: any[],
+function injectValues<T>(
+  values: T[],
   minBest = true,
   color = true,
-  mapper?: (value: any) => any
-) {
-  const { minIdx, maxIdx } = argminmax(values)
-  if (mapper) {
-    values = values.map(mapper)
-  }
+  mapper?: (value: T) => string
+): string[] {
+  const { minIdx, maxIdx } = argminmax<T>(values)
+  const result: string[] = mapper ? values.map(mapper) : values.map(String)
   if (color) {
     const minColor = minBest ? chalk.green : chalk.red
     const maxColor = minBest ? chalk.red : chalk.green
-    values[minIdx] = minColor(values[minIdx])
-    values[maxIdx] = maxColor(values[maxIdx])
+    result[minIdx] = minColor(result[minIdx])
+    result[maxIdx] = maxColor(result[maxIdx])
   }
-  return values
+  return result
 }
 
-function capitalize(str: string) {
+function capitalize(str: string): string {
   return str.charAt(0).toUpperCase() + str.slice(1)
+}
+
+export async function getRepositoryDetails(
+  fullname: string
+): Promise<Repository> {
+  const res = await fetch(`https://api.github.com/repos/${fullname}`)
+  if (parseInt(res.headers.get('x-ratelimit-remaining') as string, 10) === 0) {
+    const resetFrom = new Date(
+      parseInt(res.headers.get('x-ratelimit-reset') as string, 10) * 1000
+    )
+    throw new Error(
+      `Rate limit exceeded. It will be reset in ${formatDistanceToNow(
+        resetFrom
+      )}`
+    )
+  }
+  if (res.status === 404) {
+    throw new Error(`Repository not found for ${fullname}`)
+  }
+
+  const json = await res.json()
+
+  // tslint:disable:object-literal-sort-keys
+  return {
+    repository: json.full_name,
+    language: json.language,
+    stars: json.stargazers_count,
+    issues: json.open_issues_count, // issues + PRs
+    forks: json.forks_count,
+    watches: json.subscribers_count,
+    size: json.size,
+    updated: parseISO(json.pushed_at).getTime(),
+    created: parseISO(json.created_at).getTime(),
+    owner: json.owner.type,
+  }
+  // tslint:enable:object-literal-sort-keys
+}
+
+function CLITable(arr: unknown[]): string {
+  const table = new Table()
+  table.push(...arr)
+  return table.toString()
+}
+
+export function generateTable(
+  repos: Repository[],
+  markdownMode: boolean
+): string {
+  const keys = Object.keys(repos[0]) as Array<keyof Repository>
+  const contents = keys.map((propKey: keyof Repository): string[] => {
+    const extractedValues: Array<Repository[typeof propKey]> = repos.map(
+      (repo): Repository[typeof propKey] => repo[propKey]
+    )
+
+    let result: string[] = []
+    if (['stars', 'watches', 'forks', 'issues'].includes(propKey)) {
+      result = injectValues(extractedValues, false, !markdownMode)
+    } else if (propKey === 'size') {
+      result = injectValues(
+        extractedValues as number[],
+        true,
+        !markdownMode,
+        (kb: number): string => bytes(kb * 1024)
+      )
+    } else if (propKey === 'updated') {
+      result = injectValues(
+        extractedValues as number[],
+        false,
+        !markdownMode,
+        formatDistanceToNow
+      )
+    } else if (propKey === 'created') {
+      result = injectValues(
+        extractedValues as number[],
+        false,
+        false,
+        formatDistanceToNow
+      )
+    } else {
+      result = extractedValues as string[]
+    }
+    return [capitalize(propKey), ...result]
+  })
+
+  const table = markdownMode ? MarkdownTable(contents) : CLITable(contents)
+  return table
 }
